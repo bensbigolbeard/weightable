4610559c51c69640cab1e7317c30c1edb6cc494f
Casey McNeil <caseymcneil@gmail.com>
2013-12-27 16:16:13 -0600

    Add temp pic_url text to user#show page to test FB image import.

--- weightable_practice_scaffolding/app/models/user.rb
+++ weightable_practice_scaffolding/app/models/user.rb
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
 		        user = User.create(name:auth.extra.raw_info.name,
 		                            provider:auth.provider,
 		                            uid:auth.uid,
+		                            pic_url:auth.info.image,
 		                            email:auth.info.email,
 		                            password:Devise.friendly_token[0,20],
 		                          )
