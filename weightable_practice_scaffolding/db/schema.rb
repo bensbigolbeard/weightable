572f8e70055be4d7d21d0a8e6130dbc34a882580
Casey McNeil <caseymcneil@gmail.com>
2013-12-27 17:28:14 -0600

    Generate migration for adding goal to user table.

--- weightable_practice_scaffolding/db/schema.rb
+++ weightable_practice_scaffolding/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227221658) do
+ActiveRecord::Schema.define(version: 20131227232508) do
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20131227221658) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "profile_pic"
+    t.integer  "goal"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
