0ae16b4f52d0aa94f99312d60759b8eef171be42
Ben White <ben.white@utexas.edu>
2013-12-28 12:16:59 -0600

    Loop through friends on root page

--- weightable_practice_scaffolding/app/controllers/friendships_controller.rb
+++ weightable_practice_scaffolding/app/controllers/friendships_controller.rb
@@ -2,5 +2,9 @@ class FriendshipsController < ApplicationController
 
 	def create
 		@friendship = current_user.friendships.create(friend_id: params[:friend_id])
+    @new_user = User.find_by_id(params[:friend_id])
+    @inverse_friendship = @new_user.friendships.create(friend_id: current_user.id)
+
+    redirect_to root_path
 	end
 end
