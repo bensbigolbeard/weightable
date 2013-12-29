11452f52798fe51bb772b315693e26287303a876
Casey McNeil <caseymcneil@gmail.com>
2013-12-28 19:56:40 -0600

    Add goal weight to user#show page

--- weightable_practice_scaffolding/app/controllers/users_controller.rb
+++ weightable_practice_scaffolding/app/controllers/users_controller.rb
@@ -78,6 +78,6 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
 
   def user_params
-    params.require(:user).permit(:name, :provider, :uid, :profile_pic)
+    params.require(:user).permit(:name, :provider, :uid, :profile_pic, :goal)
   end
 end
