96046775bec98d94ac0efdcaf7b6392b26bfcbaf
MakerSquare Student <students@themakersquare.com>
2013-12-29 15:17:58 -0600

    Create Comment model and associations.

--- weightable_practice_scaffolding/app/models/weigh_in.rb
+++ weightable_practice_scaffolding/app/models/weigh_in.rb
@@ -2,4 +2,5 @@ class WeighIn < ActiveRecord::Base
 	default_scope order: 'created_at DESC'
 	belongs_to :user
 	validates :weight, presence: true
+	has_many :comments
 end
--- weightable_practice_scaffolding/db/schema.rb
+++ weightable_practice_scaffolding/db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227232508) do
+ActiveRecord::Schema.define(version: 20131229211145) do
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "weigh_in_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["weigh_in_id"], name: "index_comments_on_weigh_in_id"
 
   create_table "friendships", force: true do |t|
     t.integer  "user_id"
