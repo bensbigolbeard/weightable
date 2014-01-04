6f8c9c7daa2a1c3cf12ba8a92b45d10ece2c5d68
Casey McNeil <caseymcneil@gmail.com>
2014-01-03 18:39:20 -0600

    Add notification to the navbar that lists the number of new weigh-ins since last login

--- weightable_practice_scaffolding/app/controllers/application_controller.rb
+++ weightable_practice_scaffolding/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
-before_action :get_user_list
+before_action :get_user_list, :check_feed_notification
 
 
 def get_user_list
@@ -15,6 +15,22 @@ def get_user_list
       }
   end 
 end
+
+def check_feed_notification
+  @user = current_user
+    if @user
+    @recent_weigh_ins = []
+    @user.friends.each do |friend|
+      friend.weigh_ins.each do |weigh|
+        if weigh.created_at > @user.last_sign_in_at
+          @recent_weigh_ins << weigh
+        end
+      end
+    end
+    @notifications = @recent_weigh_ins.count
+  end
+end
+
 
 protected
 
