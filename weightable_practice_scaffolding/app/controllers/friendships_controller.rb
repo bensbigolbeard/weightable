59043fde31d6115eb88623435c8ef2b7de2ef4f0
bensbigolbeard <ben.white@utexas.edu>
2014-01-05 16:30:38 -0600

    Complete all user page with user cards

--- weightable_practice_scaffolding/app/controllers/friendships_controller.rb
+++ weightable_practice_scaffolding/app/controllers/friendships_controller.rb
@@ -7,7 +7,7 @@ class FriendshipsController < ApplicationController
   		@friendship = current_user.friendships.create(friend_id: params[:friend_id])
       @new_user = User.find_by_id(params[:friend_id])
       @inverse_friendship = @new_user.friendships.create(friend_id: current_user.id)
-      redirect_to root_path
+      redirect_to :back
     end
 	end
 
@@ -15,6 +15,6 @@ class FriendshipsController < ApplicationController
     @friendship = current_user.friendships.find_by_friend_id(params[:id])
     @friendship.destroy
     flash[:notice] = "Your friendship with #{User.find(params[:id]).name} has been removed."
-    redirect_to current_user
+    redirect_to :back
   end
 end
