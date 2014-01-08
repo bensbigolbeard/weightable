0caa714bca03d01d28015e11d7fd1e950c2be738
bensbigolbeard <ben.white@utexas.edu>
2014-01-08 15:40:03 -0600

    Add helper methods for user_content and visitor content as well as a validator to check if @user == current_user

--- weightable_practice_scaffolding/app/helpers/application_helper.rb
+++ weightable_practice_scaffolding/app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def current_user_content(&block)
+    capture(&block) if @user == current_user
+  end
+
+  def user_content(&block)
+    capture(&block) if current_user
+  end
+
+  def visitor_content(&block)
+    capture(&block) unless current_user
+  end
 end
